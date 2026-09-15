c4afd1a46e563d0c2c0164deb266d9d76fc14a5c
Muhammad Ibrahim <ranaibrahim453@gmail.com>
2026-09-15 12:32:54 +0500

    cors origin changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const API_PREFIX = "/api/v1";
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
 );
